8a456ef55ae3e33b4feb3f3adc8ddbde00995269
MingJu Sun <20104599@mail.wit.ie>
2024-03-17 05:22:01 +0000

    Lamdba B-post movie review(no validation).

--- lib/ds-assignment1-stack.ts
+++ lib/ds-assignment1-stack.ts
@@ -12,10 +12,11 @@ import * as apig from "aws-cdk-lib/aws-apigateway";
 export class DsAssignment1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
     const reviewsTable = new dynamodb.Table(this, "reviewsTable", {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "MovieId", type: dynamodb.AttributeType.NUMBER },
-      sortKey: { name: "ReviewDate", type: dynamodb.AttributeType.STRING },     // Adds a sort key to create a composite key
+      sortKey: { name: "ReviewerName", type: dynamodb.AttributeType.STRING },     // Adds a sort key to create a composite key
       removalPolicy: cdk.RemovalPolicy.DESTROY,                                 // (allows a single MovieId to have multiple reviews [one per date])
       tableName: "Reviews",                                                     // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_dynamodb.Table.html
     });
@@ -37,7 +38,7 @@ export class DsAssignment1Stack extends cdk.Stack {
       }),
     });
 
-    const getMovieReviews = new lambdanode.NodejsFunction(
+    const getMovieReviewsFn = new lambdanode.NodejsFunction(
       this,
       "GetMovieReviewsFn",
       {
@@ -53,7 +54,20 @@ export class DsAssignment1Stack extends cdk.Stack {
       }
     )
 
-    reviewsTable.grantReadData(getMovieReviews)
+    const newReviewFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda/addReview.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: reviewsTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
+    reviewsTable.grantReadData(getMovieReviewsFn)
+    reviewsTable.grantReadWriteData(newReviewFn)
 
     const api = new apig.RestApi(this, "RestAPI", {
       description: "Assignment 1 API",
@@ -71,12 +85,18 @@ export class DsAssignment1Stack extends cdk.Stack {
 
     const moviesEndpoint = api.root.addResource("movies");
 
+    const reviewsEndpoint = moviesEndpoint.addResource("reviews")
+    reviewsEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newReviewFn, { proxy: true })
+    )
+
     const movieIdEndpoint = moviesEndpoint.addResource("{movieId}");
 
     const movieReviewsEndpoint = movieIdEndpoint.addResource("reviews");
     movieReviewsEndpoint.addMethod(
       "GET",
-      new apig.LambdaIntegration(getMovieReviews, { proxy: true })
+      new apig.LambdaIntegration(getMovieReviewsFn, { proxy: true })
     )
 
   }
